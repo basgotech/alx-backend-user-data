1f1db8cf2db9f25e79788d591acefcf7f646d885
Basliel Tegegn <basgolilo5@gmail.com>
2024-07-31 23:31:12 -0700

    add

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,12 +8,6 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """
     Hashes a password using bcrypt.
-
-    Args:
-        password (str): The password to hash.
-
-    Returns:
-        bytes: The hashed password.
     """
     enc = password.encode()
     hash_tble = bcrypt.hashpw(enc, bcrypt.gensalt())
@@ -24,13 +18,6 @@ def hash_password(password: str) -> bytes:
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     Checks if the provided password matches the hashed password.
-
-    Args:
-        hashed_password (bytes): The hashed password.
-        password (str): The password to check.
-
-    Returns:
-        bool: True if the password matches the hashed password, False otherwise.
     """
     istru = False
     encoded_data = password.encode()
