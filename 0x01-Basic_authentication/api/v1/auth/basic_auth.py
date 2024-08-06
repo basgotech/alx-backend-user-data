8771f851f53d234f002580ddb8854c9af626f615
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-06 09:03:51 -0700

    fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,3 +1,4 @@
+root@04a634393431:~/alx-backend-user-data/0x01-Basic_authentication/api/v1/auth# cat basic_auth.py
 #!/usr/bin/env python3
 """Module for basic authentication
 """
@@ -28,14 +29,11 @@ class BasicAuth(Auth):
             str: The Base64 part of the Authorization header, or None if the
             header is invalid.
         """
-        
         if authorization_header is None or not \
                 isinstance(authorization_header, str):
             return None
-
         if not authorization_header.startswith("Basic "):
             return None
-        
         return authorization_header.split("Basic ")[1].strip()
 
     def decode_base64_authorization_header(
@@ -53,16 +51,13 @@ class BasicAuth(Auth):
         
         if base64_authorization_header is None:
             return None
-        
         if not isinstance(base64_authorization_header, str):
             return None
-        
         try:
             decoded = base64.b64decode(
                 base64_authorization_header,
                 validate=True
             )
-            
             return decoded.decode('utf-8')
         except (binascii.Error, UnicodeDecodeError):
             return None
@@ -79,12 +74,10 @@ class BasicAuth(Auth):
         
         if decoded_header is None or not isinstance(decoded_header, str):
             return None, None
-        
         try:
             email, password = decoded_header.split(':', 1)
         except ValueError:
             return None, None
-        
         return email, password
 
     def user_object_from_credentials(
@@ -103,19 +96,14 @@ class BasicAuth(Auth):
         if not all(map(lambda x: isinstance(x, str), (user_email, user_pwd))):
             return None
         try:
-            
             user = User.search(attributes={'email': user_email})
         except Exception:
             return None
-        
         if not user:
             return None
-        
         user = user[0]
-        
         if not user.is_valid_password(user_pwd):
             return None
-        
         return user
 
     def current_user(self, request=None) -> TypeVar('User'):
