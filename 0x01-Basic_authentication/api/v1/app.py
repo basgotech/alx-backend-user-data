65d052a1dbcc9afad1731f73cf5311b9e3cf5c4a
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-06 09:30:50 -0700

    fix the app

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,6 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-# Create a variable auth initialized to None after the CORS definition
 auth = None
 
 
