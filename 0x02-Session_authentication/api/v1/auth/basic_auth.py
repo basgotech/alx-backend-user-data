669695cd167e1e7b8e078d847b671d870fa20e53
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-08 08:35:54 -0700

    fix

--- 0x02-Session_authentication/api/v1/auth/basic_auth.py
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -1,119 +1,116 @@
 #!/usr/bin/env python3
-"""Module for basic authentication
+""" Module of Basic Authentication
 """
-import base64
-import binascii
-from typing import Tuple, TypeVar
-
+from api.v1.auth.auth import Auth
+from base64 import b64decode
 from models.user import User
-
-from .auth import Auth
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
-    """Basic authentication class.
+    """ Basic Authentication Class """
 
-    Args:
-        Auth (type): Class inherited from.
-    """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extract Base 64 Authorization Header """
 
-    def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
-        """Extracts the Base64 part of the Authorization header.
-
-        Args:
-            authorization_header (str): The Authorization header string.
-
-        Returns:
-            str: The Base64 part of the Authorization header, or None if the
-            header is invalid.
-        """
-        if authorization_header is None or not \
-                isinstance(authorization_header, str):
+        if authorization_header is None:
             return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
         if not authorization_header.startswith("Basic "):
             return None
-        return authorization_header.split("Basic ")[1].strip()
 
-    def decode_base64_authorization_header(
-            self, base64_authorization_header: str) -> str:
-        """Decodes the Base64 string `base64_authorization_header` and
-        returns the decoded value as a UTF8 string.
+        encoded = authorization_header.split(' ', 1)[1]
 
-        Args:
-            base64_authorization_header (str): A Base64 encoded string to be
-            decoded.
+        return encoded
 
-        Returns:
-            str: The decoded value as a UTF8 string.
-        """
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ Decodes the value of a base64 string """
         if base64_authorization_header is None:
             return None
         if not isinstance(base64_authorization_header, str):
             return None
+
         try:
-            decoded = base64.b64decode(
-                base64_authorization_header,
-                validate=True
-            )
-            return decoded.decode('utf-8')
-        except (binascii.Error, UnicodeDecodeError):
+            encoded = base64_authorization_header.encode('utf-8')
+            decoded64 = b64decode(encoded)
+            decoded = decoded64.decode('utf-8')
+        except BaseException:
             return None
 
-    def extract_user_credentials(self, decoded_header: str) -> Tuple[str, str]:
-        """Extract the user email and password from the decoded header string.
+        return decoded
 
-        Args:
-            decoded_header (str): A decoded header string.
-
-        Returns:
-            Tuple[str, str]: Tuple containing the user email and password.
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
         """
-        if decoded_header is None or not isinstance(decoded_header, str):
-            return None, None
-        try:
-            email, password = decoded_header.split(':', 1)
-        except ValueError:
-            return None, None
-        return email, password
-
-    def user_object_from_credentials(
-            self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        """Returns the User instance based on the email and password.
-
-        Args:
-            user_email (str): The user's email.
-            user_pwd (str): The user's password.
-
-        Returns:
-            User: The User instance or None if the user is not found or the
-            password is invalid.
+        Returns the user email and password from the
+        Base64 decoded value
         """
-        if not all(map(lambda x: isinstance(x, str), (user_email, user_pwd))):
+
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        credentials = decoded_base64_authorization_header.split(':', 1)
+
+        return credentials[0], credentials[1]
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        Returns the User instance based on his
+        email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
             return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
         try:
-            user = User.search(attributes={'email': user_email})
+            found_users = User.search({'email': user_email})
         except Exception:
             return None
-        if not user:
-            return None
-        user = user[0]
-        if not user.is_valid_password(user_pwd):
-            return None
-        return user
+
+        for user in found_users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Retrieves the User instance for a request.
-
-        Args:
-            request (:obj:`Request`, optional): The request object. Defaults
-            to None.
-
-        Returns:
-            User: The User instance based on the request.
-        """
+        """ overloads Auth and retrieves the User instance for a request """
         auth_header = self.authorization_header(request)
-        b64_auth_header = self.extract_base64_authorization_header(auth_header)
-        dec_header = self.decode_base64_authorization_header(b64_auth_header)
-        user_email, user_pwd = self.extract_user_credentials(dec_header)
-        return self.user_object_from_credentials(user_email, user_pwd)
+
+        if not auth_header:
+            return None
+
+        encoded = self.extract_base64_authorization_header(auth_header)
+
+        if not encoded:
+            return None
+
+        decoded = self.decode_base64_authorization_header(encoded)
+
+        if not decoded:
+            return None
+
+        email, pwd = self.extract_user_credentials(decoded)
+
+        if not email or not pwd:
+            return None
+
+        user = self.user_object_from_credentials(email, pwd)
+
+        return user
--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -1,46 +1,12 @@
 #!/usr/bin/env python3
-""" Module of Index views
+""" DocDocDocDocDocDoc
 """
-from flask import jsonify, abort
-from api.v1.views import app_views
+from flask import Blueprint
 
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
-    """
-    return jsonify({"status": "OK"})
+from api.v1.views.index import *
+from api.v1.views.users import *
+from api.v1.views.session_auth import *
 
-
-@app_views.route('/stats/', strict_slashes=False)
-def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
-    """
-    from models.user import User
-    stats = {}
-    stats['users'] = User.count()
-    return jsonify(stats)
-
-
-@app_views.route('/unauthorized/', strict_slashes=False, methods=['GET'])
-def unauthorized_endpoint() -> None:
-    """Endpoint that raises a 401 error.
-
-    Returns:
-        None: None.
-    """
-    abort(401)
-
-
-@app_views.route('/forbidden/', strict_slashes=False, methods=['GET'])
-def forbidden_endpoint() -> None:
-    """Endpoint that raises a 403 error.
-
-    Returns:
-        None: None.
-    """
-    abort(403)
+User.load_from_file()
