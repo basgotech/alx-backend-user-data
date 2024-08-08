c0c7de7e82da31ab20a1f641d5ce297edf9b2349
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-08 08:49:22 -0700

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,31 +2,34 @@
 """
 Route module for the API
 """
-
-
-import os
 from os import getenv
-from typing import Tuple
 
-from flask import Flask, abort, jsonify, request
-from flask_cors import CORS, cross_origin
-
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
 
-
-# Update api/v1/app.py for using BasicAuth class instead of Auth depending
-auth_type = getenv('AUTH_TYPE', 'default')
-if auth_type == "basic_auth":
-    auth = BasicAuth()
-else:
+if getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
     auth = Auth()
+elif getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif getenv('AUTH_TYPE') == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif getenv('AUTH_TYPE') == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
@@ -37,54 +40,41 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error: Exception) -> Tuple[jsonify, int]:
-    """Error handler for unauthorized requests.
-
-    Args:
-        error (Exception): The error raised.
-
-    Returns:
-        Tuple[jsonify, int]: JSON response with the error message and a 401
-        status code.
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error: Exception) -> Tuple[jsonify, int]:
-    """Error handler for unauthorized requests.
-
-    Args:
-        error (Exception): The error raised.
-
-    Returns:
-        Tuple[jsonify, int]: JSON response with the error message and a 401
-        status code.
+def forbidden_error(error) -> str:
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def handle_request():
+def before_request() -> None:
+    """ Filter for request
     """
-    Handle the request by checking for authentication and authorization.
-    """
-    if auth is None:
-        return
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    auth_header = auth.authorization_header(request)
-    if auth_header is None:
-        abort(401)
-    user = auth.current_user(request)
-    if user is None:
-        abort(403)
+    request_path_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/']
+    if auth:
+        if auth.require_auth(request.path, request_path_list):
+            if auth.authorization_header(
+                    request) is None and auth.session_cookie(request) is None:
+                abort(401)
+            request.current_user = auth.current_user(request)
+            if auth.current_user(request) is None:
+                abort(403)
+            if request.current_user is None:
+                abort(403)
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
