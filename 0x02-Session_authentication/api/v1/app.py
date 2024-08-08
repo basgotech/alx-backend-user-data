33b927ecffade09f76fdba9f6fc6b8fdee15dae2
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-08 08:58:53 -0700

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -3,31 +3,31 @@
 Route module for the API
 """
 from os import getenv
+
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-# Check authentication type
-auth_type = getenv('AUTH_TYPE')
-if auth_type == 'auth':
+
+if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-if auth_type == 'basic_auth':
+elif getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-if auth_type == 'session_auth':
+elif getenv('AUTH_TYPE') == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-if auth_type == 'session_exp_auth':
+elif getenv('AUTH_TYPE') == 'session_exp_auth':
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
-if auth_type == 'session_db_auth':
+elif getenv('AUTH_TYPE') == 'session_db_auth':
     from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
 
@@ -40,48 +40,41 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized access handler
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error):
-    """ Forbidden access handler
+def forbidden_error(error) -> str:
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_request():
-    """ Actions before serving a request
+def before_request() -> None:
+    """ Filter for request
     """
-    # Authorization parameter not set
-    if not auth:
-        return
-    # Check if path doesn't require authorization
-    if not auth.require_auth(request.path, ['/api/v1/status/',
-                                            '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/',
-                                            '/api/v1/auth_session/login/']):
-        return
-
-    # Check for existence of an authentication scheme
-    # Either: session authentication and basic auth
-    session_cookie = auth.session_cookie(request)
-    auth_header = auth.authorization_header(request)
-    if not session_cookie and not auth_header:
-        abort(401)
-
-    # Check if current user is authorized to access the route
-    current_user = auth.current_user(request)
-    if not current_user:
-        abort(403)
-    setattr(request, "current_user", current_user)
+    request_path_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/']
+    if auth:
+        if auth.require_auth(request.path, request_path_list):
+            if auth.authorization_header(
+                    request) is None and auth.session_cookie(request) is None:
+                abort(401)
+            request.current_user = auth.current_user(request)
+            if auth.current_user(request) is None:
+                abort(403)
+            if request.current_user is None:
+                abort(403)
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
