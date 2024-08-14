8edd4962cf6e446df5a5441360017112c1a120c4
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-14 09:31:57 -0700

    fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""DB module
+"""DB module for the project
 """
 from sqlalchemy import create_engine, tuple_
 from sqlalchemy.exc import InvalidRequestError
@@ -10,7 +10,7 @@ from user import Base, User
 
 
 class DB:
-    """DB class
+    """DB class initilize
     """
 
     def __init__(self) -> None:
@@ -23,7 +23,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """session start
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -31,8 +31,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Creates new User instance and
-            saves them to the database.
+        """ Creates new User to db
             Args:
                 - email
                 - hashed_password
@@ -41,13 +40,13 @@ class DB:
         """
         session = self._session
         try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            session.add(new_user)
+            add_user = User(email=email, hashed_password=hashed_password)
+            session.add(add_user)
             session.commit()
         except Exception:
             session.rollback()
-            new_user = None
-        return new_user
+            add_user = None
+        return add_user
 
     def find_user_by(self, **kwargs) -> User:
         """ Find user by a given attribute
